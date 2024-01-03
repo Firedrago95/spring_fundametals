8291a693d5b312c95710bbb926845c40330b78f5
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-03 09:30:25 +0900

    순수 자바테스트

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,11 +1,11 @@
 package hello.core.member;
 
 public class Member {
-    private Long id;
+    private long id;
     private String name;
     private Grade grade;
 
-    public Member(Long id, String name, Grade grade) {
+    public Member(long id, String name, Grade grade) {
         this.id = id;
         this.name = name;
         this.grade = grade;
