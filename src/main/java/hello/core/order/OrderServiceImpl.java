f772de220e682778badd2fed2205315a02903bec
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-08 10:44:50 +0900

    생성자 주빕과 수정자 주입 실행순서

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -17,16 +17,19 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("OrderServiceImpl.setMemberRepository");
         this.memberRepository = memberRepository;
     }
 
     @Autowired
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
     }
 
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
