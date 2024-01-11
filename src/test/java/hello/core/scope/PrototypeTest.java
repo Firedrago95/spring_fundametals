cbcce4a3e3f8e593cdfa5856318833ebc0ea00e9
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-11 11:50:00 +0900

    prototype 빈ã 스코프 테스트

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+
+        System.out.println("find PrototypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("find PrototypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean{
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
